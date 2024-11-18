f54c57873e209ed793559d761982670daaeaee40
Rahei <Rahei@RAHEIMSPC>
2024-11-18 13:26:31 -0500

    Add project files.

---
+++ isleapyear/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+
+namespace isleapyear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine($"{year} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year");
+            }
+        }
+        static bool IsLeapYear(int year)
+        {
+            return( year % 4 == 0 && year % 100 != 0)||(year % 400 ==0);
+            
+          
+        }
+            
+    }
+}
+        
+      
+
+
